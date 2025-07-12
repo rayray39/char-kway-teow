df81c8954fad37e7d2cc983ab0d7e48ba861d323
rayray39 <raynerlimfy@gmail.com>
2025-07-12 13:45:46 +0800

    main.tsx: add navigation routes to sign in and app

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,19 @@ import './index.css'
 import App from './App.tsx'
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from './SignIn.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <MantineProvider>
-            <App />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<SignIn />} />
+                    <Route path="/app" element={<App />} />
+                </Routes>
+            </BrowserRouter>
         </MantineProvider>
     </StrictMode>,
 )

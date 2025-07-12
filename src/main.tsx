2eccb26fe7fdfcf16eb786963b854b405f2337c4
rayray39 <raynerlimfy@gmail.com>
2025-07-12 16:00:47 +0800

    update app route to be protected using jwt auth

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from './SignIn.tsx';
+import PrivateRoute from './PrivateRoute.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
@@ -14,7 +15,11 @@ createRoot(document.getElementById('root')!).render(
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<SignIn />} />
-                    <Route path="/app" element={<App />} />
+                    <Route path="/app" element={
+                        <PrivateRoute>
+                            <App />
+                        </PrivateRoute>
+                    } />
                 </Routes>
             </BrowserRouter>
         </MantineProvider>
